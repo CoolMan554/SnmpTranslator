4c0f52599c7f74749815dfb82d845963561f0d29
Конычев Дмитрий Сергеевич <KonychevDS@morion.ru>
2024-06-09 16:30:51 +0500

    Создание и инициализация базы

---
+++ SnmpTranslator/dblayerbase.cpp
@@ -0,0 +1,52 @@
+#include "dblayerbase.h"
+
+DbLayerBase::DbLayerBase(QObject *parent)
+    : QObject{parent}
+{
+    db.addDatabase("QPSQL");
+    db.setHostName(this->getDbHost());
+    db.setDatabaseName(this->getDbName());
+    db.setPort(this->getDbPort());
+    db.setUserName(this->getDbName());
+    db.setPassword(this->getDbPass());
+
+    if(!db.open())
+    {
+        qDebug() << "DbLayerBase::" << db.lastError().text();
+    }
+    else qDebug() << "DbLayerBase:: Новое соединение СУБД" << db.connectionName();
+}
+DbLayerBase::~DbLayerBase()
+{
+    qDebug() << "DbLayerBase:: Соединение закрыто" << db.connectionName();
+    db.close();
+}
+
+bool DbLayerBase::isDbReady() const
+{
+    return getDb().isOpen();
+}
+
+void DbLayerBase::insertSqlCommand(const QString &data)
+{
+    QString strCommand = QString("INSERT INTO (eventin, additionalinfo) VALUES (:eventin, :additionalinfo)");
+    preparedQuery(strCommand, data);
+}
+
+void DbLayerBase::preparedQuery(const QString &query, const QString &data)
+{
+    if(!isDbReady())
+    {
+        qDebug() << "DbLayerBase:: Нет соединения с базой" << db.lastError().text();
+    }
+    QSqlQuery q(getDb());
+    q.prepare(query);
+    q.bindValue(":eventin", QDateTime::currentDateTime().toTimeZone(QTimeZone::systemTimeZone()).toString(Qt::ISODate));
+    q.bindValue(":additionalinfo", data);
+    q.exec();
+
+    if(!q.isValid() || q.lastError().isValid())
+    {
+        qDebug() << "DbLayerBase::execQuery Ошибка в запросе " << q.lastError().text();
+    }
+}
---
+++ SnmpTranslator/dblayerbase.h
@@ -0,0 +1,39 @@
+#ifndef DBLAYERBASE_H
+#define DBLAYERBASE_H
+
+#include <QObject>
+#include <QDebug>
+#include <QDateTime>
+#include <QTimeZone>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlQuery>
+
+class DbLayerBase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DbLayerBase(QObject *parent = nullptr);
+    ~DbLayerBase();
+    QString getDbHost() const { return DB_HOST; }
+    int     getDbPort() const { return DB_PORT; }
+    QString getDbName() const { return DB_NAME; }
+    QString getDbUser() const { return DB_USER; }
+    QString getDbPass() const { return DB_PASS; }
+    QSqlDatabase getDb() const { return db; }
+    bool isDbReady() const;
+    void insertSqlCommand(const QString &data);
+private:
+    QSqlDatabase db;
+    QString DB_HOST = QStringLiteral("127.0.0.1");
+    int DB_PORT = 5432;
+    QString DB_NAME = QStringLiteral("snmp");
+    QString DB_USER = QStringLiteral("snmp");
+    QString DB_PASS = QStringLiteral("snmp");
+    void preparedQuery(const QString &query, const QString &data);
+
+signals:
+
+};
+
+#endif // DBLAYERBASE_H
